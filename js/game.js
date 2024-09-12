c0910d7d62c7fcb2c9a26da9783caa01d404bba0
MatheGenie27 <78275290+MatheGenie27@users.noreply.github.com>
2024-09-12 13:43:40 +0200

    classes

---
+++ js/game.js
@@ -0,0 +1,13 @@
+
+let canvas;
+let ctx;
+
+let world = new World();
+
+function init(){
+    canvas = document.getElementById('myCanvas');
+    ctx = canvas.getContext('2d');
+
+
+    console.log("My character is", world.character);
+}
---
+++ js/models/character.class.js
@@ -0,0 +1,12 @@
+class Character extends MovableObject{
+
+
+
+
+
+
+jump(){
+    
+}
+
+}
