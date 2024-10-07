1b4f11bbfefb87c65c70ee21ce84bcd31de86851
MatheGenie27 <78275290+MatheGenie27@users.noreply.github.com>
2024-10-07 11:44:43 +0200

    added classes

---
+++ js/models/bottle.class.js
@@ -0,0 +1,3 @@
+class bottle extends collectableObject {
+    
+}
--- js/models/chick.class.js
+++ js/models/chick.class.js
@@ -1,5 +1,4 @@
-class Chicken extends MovableObject{
-
+class Chick extends MovableObject {
     x = 500;
     y = 350;
     width= 50;
@@ -8,19 +7,19 @@ class Chicken extends MovableObject{
     
 
     IMAGES_WALKING=[
-        '../img/3_enemies_chicken/chicken_normal/1_walk/1_w.png',
-        '../img/3_enemies_chicken/chicken_normal/1_walk/2_w.png',
-        '../img/3_enemies_chicken/chicken_normal/1_walk/3_w.png'
+        '../img/3_enemies_chicken/chicken_small/1_walk/1_w.png',
+        '../img/3_enemies_chicken/chicken_small/1_walk/2_w.png',
+        '../img/3_enemies_chicken/chicken_small/1_walk/3_w.png'
     ]
 
 
     constructor(){
         super();
-        super.loadImage('../img/3_enemies_chicken/chicken_normal/1_walk/1_w.png');
+        super.loadImage('../img/3_enemies_chicken/chicken_small/1_walk/1_w.png');
         super.loadImages(this.IMAGES_WALKING);
 
-        this.x = 500 + Math.random()*1500;
-        this.speed = 0.15 + Math.random()*0.25;
+        this.x = 1500 + Math.random()*500;
+        this.speed = 0.25 + Math.random()*0.25;
         this.animate();
     }
 
@@ -37,7 +36,5 @@ class Chicken extends MovableObject{
        
 
     }
-
-
 
 }
